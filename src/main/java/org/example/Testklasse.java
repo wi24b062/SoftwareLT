62b9014dab93cabc1bf36622bfa4ed7e48ef4795
Teodi7 <wi24b169@technikum-wien.at>
2025-06-10 09:48:53 +0200

    Probiere Commit and Push

---
+++ src/main/java/org/example/Testklasse.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Testklasse {
+    public static void main(String[] args) {
+        System.out.println("Test Commit. Ich möchte damit das committen und pushen testen, wird danach wieder gelöscht!");
+    }
+}
