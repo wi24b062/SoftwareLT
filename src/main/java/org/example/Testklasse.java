5a7274d0736e3273f5abfea8c2f362d292f8dfb8
omar-ft <wi24b185@technikum-wien.at>
2025-06-30 19:54:27 +0200

    Actually remove Testklasse.java

--- src/main/java/org/example/Testklasse.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class Testklasse {
-    public static void main(String[] args) {
-        System.out.println("Test Commit. Ich möchte damit das committen und pushen testen, wird danach wieder gelöscht!");
-    }
-}
